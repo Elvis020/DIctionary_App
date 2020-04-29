01d61693d3c4c92f8769b9c4fae6d9c23af9bd68
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-04-29 16:53:34 +0000

    First Batch

---
+++ Dictionary_app.py
@@ -0,0 +1,34 @@
+# import json
+# help(json.load)
+# data = json.load(open("/home/spacey/Projects_with_Python/App_1/2.2 data.json"))
+# print(type(data))
+# print(data["access"])
+#Write a function that takes an input and returns a value
+def translate(w):
+    import json
+    import difflib
+    from difflib import get_close_matches
+    data = json.load(open("/home/spacey/Projects_with_Python/App_1/2.2 data.json"))
+
+    if w in data.keys():
+        return print(f"""{w}: {data[w]}""")
+    else:
+        similar_words = get_close_matches(w, data.keys())
+        if len(similar_words) > 0:
+            simi = input("""Do you mean "%s".Enter y if Yes or n if No: """ % similar_words[0]).upper()
+            if simi == "Y":
+                return print(f"""{similar_words[0]}:{data[similar_words[0]]}""")
+            else:
+                return print("The word " + "\"" + w + "\"" + " doesn't exist, please double check your spelling.")
+
+word = input("Plaese enter a word: ").casefold()
+output = translate(word)
+for n in range(output):
+    print(output)
+# Note: The difflib libraray was used to find the similarity between words
+# import json
+# data = json.load(open("/home/spacey/Projects_with_Python/App_1/2.2 data.json"))
+# # data.keys()
+# import difflib
+# from difflib import get_close_matches
+# print(get_close_matches("rainn", data.keys()))
